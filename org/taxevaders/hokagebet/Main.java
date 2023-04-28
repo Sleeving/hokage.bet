90a89acd4a2e7c7aa4937b7bcaa4a83669bf0c64
Sleev <59300997+Sleeving@users.noreply.github.com>
2023-04-28 12:47:21 -0400

    package

---
+++ org/taxevaders/hokagebet/Main.java
@@ -0,0 +1,6 @@
+package org.taxevaders.hokagebet;
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
---
+++ out/production/hokage.bet/README.md
@@ -0,0 +1,2 @@
+<h1>Hokage.bet</h1>
+<img height="200" src="/Users/user1/Downloads/xqc-gambling.gif" width="200"/>
